469287b642ba003cbb0f4d02acbcf603abef2f87
Koki Nishihara <snkg2450@gmail.com>
2022-04-28 22:59:26 +0900

    docstring

--- kyopro_educational_90/001/main.py
+++ kyopro_educational_90/001/main.py
@@ -2,18 +2,35 @@ N, L = list(map(int, input().split()))
 K = int(input())
 A = list(map(int, input().split()))
 
-A=sorted(A)
+A = sorted(A)
 
 left = 0
-right = L-1
+right = L - 1
 
-def check(length):
+
+def check(length) -> bool:
+    """
+    It checks if it's possible to split the array into K+1 parts, where each part is of length at most
+    length
+    
+    Args:
+      length: the length of the part
+    
+    Returns:
+      True or False
+    """
+
     part_length = 0
     count = 0
     for i in A:
-        part_length +=i
-        if(length<=part_length):
-            count+=1
-            part_length=0                        
+        part_length += i
+        if length <= part_length:
+            count += 1
+            part_length = 0
+
+    if part_length < length:
+        return False
+    return K + 1 <= count
+
 
 print(A)
