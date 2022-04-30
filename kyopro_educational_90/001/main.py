02497b0bace4acd3b1e38283215698698599fb6f
Koki Nishihara <snkg2450@gmail.com>
2022-04-30 19:12:19 +0900

    debug

--- kyopro_educational_90/001/main.py
+++ kyopro_educational_90/001/main.py
@@ -1,3 +1,5 @@
+import pdb
+
 N, L = list(map(int, input().split()))
 K = int(input())
 A = list(map(int, input().split()))
@@ -12,10 +14,10 @@ def check(length) -> bool:
     """
     It checks if it's possible to split the array into K+1 parts, where each part is of length at most
     length
-    
+
     Args:
       length: the length of the part
-    
+
     Returns:
       True or False
     """
@@ -28,16 +30,21 @@ def check(length) -> bool:
             count += 1
             part_length = 0
 
-    if part_length < length:
-        return False
+    pdb.set_trace()
+    # if part_length < length:
+    #     return False
     return K + 1 <= count
 
 
 print(A)
 # 二分探索
 while left <= right:
+
+    pdb.set_trace()
     mid = (left + right) // 2
     if check(mid):
-        right = mid - 1
+        left = mid
     else:
-        left = mid + 1
+        right = mid
+    print(left, right)
+print(left)
